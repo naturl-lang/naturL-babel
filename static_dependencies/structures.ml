8881196354f1de8f2e544b1c7f7a49dead7e6b9c
rostan <rostan.tabet@gmail.com>
2020-01-15 16:23:08 +0100

    Modified grammar.ml

--- static_dependencies/structures.ml
+++ static_dependencies/structures.ml
@@ -1,17 +1,20 @@
-type position = {row: int; column: int}
-
 type type_struct =
     Int
   | Float
   | Char
   | String
+  | Boolean
   | List of type_struct
   | Function of type_struct list * type_struct ;;
 
 
 type scope =
     Normal
+  | Current (* Used to indicate no change of the current scope*)
   | Previous
+  | ForDefinition
+  | WhileDefinition
+  | IfDefinition
   | FuncDefinition
   | FuncHeader
   | FuncBody
