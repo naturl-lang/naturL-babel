ee5b81eea93e5436a15fc8834306d9fb4d1c54b1
Argatu Vlad <vlad.argatu@epita.fr>
2020-04-15 10:37:18 +0200

    patching unvalid tests

--- tests/test_translation.ml
+++ tests/test_translation.ml
@@ -6,14 +6,14 @@ print_string "Beginning translation.ml tests... " ;;
 
 let vars = StringMap.empty ;;
 
-let%expect_test "fonction" = let context = {code = "fonction test() -> entier\nfin"; index = 0; vars; scopes = [Function ""]} in
+let%expect_test "fonction" = let context = {code = "fonction test() -> entier\ndebut\nretourner 5\nfin"; index = 0; vars; scopes = [Function ""]} in
   let translation, _ = eval_fonction context in
   print_string translation;
   [%expect {|
     def test():
-        pass |}] ;;
+        return 5|}] ;;
 
-let%expect_test "procedure" = let context = {code = "procedure test()\nfin"; index = 0; vars; scopes = [Function ""]} in
+let%expect_test "procedure" = let context = {code = "procedure test()\ndebut\nfin"; index = 0; vars; scopes = [Function ""]} in
   let translation, _ = eval_procedure context in
   print_string translation;
   [%expect {|
