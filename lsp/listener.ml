4eab78ac616bb08d801abab717d41c79ead7bb0e
rostan <rostan.tabet@gmail.com>
2020-09-15 01:50:50 +0200

    First commit for a long time. Switched to TCP protocol as a communication channel for the LSP server.

--- lsp/listener.ml
+++ lsp/listener.ml
@@ -1,27 +1,32 @@
-let handle_notification : Client_notification.t -> unit = function
+let handle_notification ic oc : Client_notification.t -> unit = function
   | Initialized -> Environment.initialize ()
-  | Exit -> let code = if Environment.is_shutdown () then 0 else 1 in exit code
+  | Exit ->
+     (try
+        flush oc;
+        close_in ic
+      with _ -> close_in_noerr ic);
+     let code = if Environment.is_shutdown () then 0 else 1 in exit code
   | TextDocumentDidOpen params -> let uri = params.textDocument.uri in
-    Environment.add_uri uri params.textDocument.text; Functions.diagnostic stdout
+                                  Environment.add_uri uri params.textDocument.text; Functions.diagnostic oc
   | TextDocumentDidChange params -> let uri = params.textDocument.uri in
-    Environment.add_uri uri (List.hd params.contentChanges).text; Functions.diagnostic stdout
+                                    Environment.add_uri uri (List.hd params.contentChanges).text; Functions.diagnostic oc
   | TextDocumentDidClose params -> Environment.remove_uri params.textDocument.uri
   | Unknown_notification _ -> ()
 
-let handle_request id : Request.t -> unit = function
-  | Shutdown -> Environment.shutdown (); Sender.send_response stdout Jsonrpc.(Response.ok id `Null)
-  | Initialize params -> Environment.set_client_capabilities params.capabilities; Sender.initialize stdout id ;
+let handle_request oc id : Request.t -> unit = function
+  | Shutdown -> Environment.shutdown (); Sender.send_response oc Jsonrpc.(Response.ok id `Null)
+  | Initialize params -> Environment.set_client_capabilities params.capabilities; Sender.initialize oc id ;
     (match params.initializationOptions with
        Some lang -> Internationalisation__Translation.set_lang_of_string lang
      | None -> ())
-  | TextDocumentDefinition params -> Functions.definition stdout id params
-  | TextDocumentCompletion params -> Functions.completion stdout id params
-  | TextDocumentFormat params -> Functions.reformat stdout id params
-  | UnknownRequest name -> Sender.send_response stdout
+  | TextDocumentDefinition params -> Functions.definition oc id params
+  | TextDocumentCompletion params -> Functions.completion oc id params
+  | TextDocumentFormat params -> Functions.reformat oc id params
+  | UnknownRequest name -> Sender.send_response oc
                              Jsonrpc.(Response.error id Response.Error.(make ~code:MethodNotFound ~message:("Unknown method " ^ name) ()))
 
 
-let rec listen ic =
+let rec listen ic oc =
   let read_content ic length =
     let rec read_content ic length accu =
     if length = 0 then
@@ -41,25 +46,26 @@ let rec listen ic =
     | Some id -> (try (match Request.of_jsonrpc jsonrpc with
         | Ok request ->
           if Environment.is_shutdown () then
-            Sender.send_response stdout
+            Sender.send_response oc
               Jsonrpc.(Response.error id Response.Error.(make ~code:InvalidRequest ~message:"The server is shut down" ()))
           else if Environment.initialized () || (function Request.Initialize _ -> true | _ -> false) request then
-            handle_request id request
+            handle_request oc id request
           else
-            Sender.send_response stdout
+            Sender.send_response oc
               Jsonrpc.(Response.error id Response.Error.(make ~code:ServerNotInitialized ~message:"Please first initialize the server" ()))
         | Error _ -> prerr_endline "error")
        with Ppx_yojson_conv_lib__Yojson_conv.Of_yojson_error _ ->
-         Sender.send_response stdout
+         Sender.send_response oc
            Jsonrpc.(Response.error id Response.Error.(make ~code:InvalidRequest ~message:"Invalid request format" ())))
     (* Notification *)
     | None -> (match Client_notification.of_jsonrpc jsonrpc with
         | Ok notification ->
-          if not (Environment.is_shutdown () && notification <> Exit) || notification = Initialized &&
-                                                                         (Environment.initialized () || !Environment.client_capabilities <> None && notification = Initialized) then
-            handle_notification notification
+           if not (Environment.is_shutdown () && notification <> Exit) ||
+                notification = Initialized &&
+                  (Environment.initialized () || !Environment.client_capabilities <> None && notification = Initialized) then
+            handle_notification ic oc notification
         | Error _ -> prerr_endline "error")
   end;
-  listen ic
+  listen ic oc
 
 let start = listen
--- lsp/server.ml
+++ lsp/server.ml
@@ -1,1 +1,16 @@
-let () = Listener.start stdin
+let setup_server fx addr port =
+  let open Unix in
+  let sockaddr = ADDR_INET (addr, port) in
+  let sock = socket PF_INET SOCK_STREAM 0 in
+  bind sock sockaddr;
+  listen sock 1;  (* An LSP sevrver only accepts one client *)
+  let fd, _ = accept sock in
+  (* Low level UNIX file descriptor to high level channels *)
+  let ic = in_channel_of_descr fd
+  and oc = out_channel_of_descr fd in
+  fx ic oc
+
+let () =
+  let port = 9131
+  and address = Unix.inet_addr_loopback in
+  setup_server Listener.start address port
