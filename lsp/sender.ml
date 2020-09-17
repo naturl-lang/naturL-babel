67b8918021cf5c25781bf39872e6661578f1d5e1
rostan <rostan.tabet@gmail.com>
2020-09-17 14:38:48 +0200

    Added log to the LSP server

--- lsp/sender.ml
+++ lsp/sender.ml
@@ -11,6 +11,7 @@ let send_packet oc packet =
   in let json = Yojson.Safe.to_string yojson
   in let content_length = String.length json in
   Header.(write oc (make ~content_length ()));
+  print_endline (String.make 100 '*' ^ "Sent message : " ^ json ^ (String.make 100 '*'));
   output_string oc (json ^ "\n");
   flush oc
 
